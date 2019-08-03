ea94e66d8d3ea748d6d31b78f3b43298c3ff768f
Mihail <bladesvet@gmail.com>
2019-08-03 12:36:28 +0300

    test

---
+++ src/test_work2/by/main/MainApp.java
@@ -0,0 +1,14 @@
+package test_work2.by.main;
+
+public class MainApp {
+
+	public MainApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
